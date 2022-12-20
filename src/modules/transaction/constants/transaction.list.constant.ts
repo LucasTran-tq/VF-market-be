80f7068a7d913d03f52022008120044c7614ea99
LucasTran <lucastrantq@gmail.com>
2022-12-20 16:41:34 +0700

    [api] get transaction list

--- src/modules/transaction/constants/transaction.list.constant.ts
+++ src/modules/transaction/constants/transaction.list.constant.ts
@@ -1,5 +1,5 @@
 export const TRANSACTION_DEFAULT_PAGE = 1;
 export const TRANSACTION_DEFAULT_PER_PAGE = 20;
 export const TRANSACTION_DEFAULT_SORT = 'createdAt@asc';
-export const TRANSACTION_DEFAULT_AVAILABLE_SORT = ['name', 'createdAt'];
-export const TRANSACTION_DEFAULT_AVAILABLE_SEARCH = ['name'];
+export const TRANSACTION_DEFAULT_AVAILABLE_SORT = ['address', 'createdAt'];
+export const TRANSACTION_DEFAULT_AVAILABLE_SEARCH = ['address'];
